9a6a1095ad48773747159e0864b213f2ec90a371
Novachrono <59332850+BLACKPARADOXz@users.noreply.github.com>
2022-04-12 23:23:04 +0300

    Delete src/Models directory

--- src/Models/tagSchema.js
+++
@@ -1,9 +0,0 @@
-const { model, Schema } = require("mongoose");
-
-const schema = new Schema({
-    guild_id: String,
-    name: String,
-    any: Array    
-})
-
-module.exports = model("SpecialRoles", schema);
